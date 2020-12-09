bcd30807f13bca11b5842b813d5fdaeefec178f4
Douglas Sena <dsena.m@hotmail.com>
2020-12-09 14:37:11 -0300

    + header

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { Nav } from './styles';
+
+function Header() {
+  return (
+    <>
+      <Nav>
+        <Link to="/">
+          <FaHome size={24} />
+        </Link>
+
+        <Link to="/">
+          <FaSignInAlt size={24} />
+        </Link>
+
+        <Link to="/">
+          <FaUserAlt size={24} />
+        </Link>
+      </Nav>
+    </>
+  );
+}
+
+export default Header;
