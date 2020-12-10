b3ed52d3a361b464b82cfc6ed1bb3786046bf130
Douglas Sena <dsena.m@hotmail.com>
2020-12-09 21:27:21 -0300

    + redux config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
 import history from './services/history';
-
+import store from './store';
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
@@ -10,12 +11,14 @@ import Routes from './routes';
 function App() {
   return (
     <>
-      <Router history={history}>
-        <Header />
-        <Routes />
-        <GlobalStyle />
-        <ToastContainer autoClose={3000} />
-      </Router>
+      <Provider store={store}>
+        <Router history={history}>
+          <Header />
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </Provider>
     </>
   );
 }
